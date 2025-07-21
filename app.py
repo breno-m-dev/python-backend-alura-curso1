9e2ab4e3f12cd7a5905d9c6d1712800595c81ba1
unknown <brenomdev@protonmail.com>
2025-07-21 15:17:02 -0300

    Aula 3 de Python  Alura back end assistida. Muda partes repetitivas em funções. Cria várias funções e melhora algumas delas.

--- app.py
+++ app.py
@@ -1,19 +1,45 @@
 from colorama import init, Fore, Style
 init(autoreset=True)
 
-import os;
+import os
 
+
+restaurantes = []
+
+def pausar():
+    input('\nAperte qualquer tecla para continuar ')
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear') #limpa a tela do terminal, indepentente se for windows, mac ou linux
+
+def mostra_subtitulo(texto):
+    print( Style.BRIGHT + Fore.LIGHTMAGENTA_EX  + texto +'\n')
 
 
 def finalizar_app():
-    os.system('cls') #limpa a tela do terminal
-    print( Style.BRIGHT + Fore.LIGHTMAGENTA_EX  + 'Encerrando o programa\n' )
+    limpar_tela()
+    mostra_subtitulo('Encerrando o programa')
 
 def cadastrar_restaurante():
-    print(  Style.BRIGHT + Fore.LIGHTMAGENTA_EX + 'Cadastrar restaurante\n' )
+    limpar_tela()
+    mostra_subtitulo('Cadastro de novos restaurantes')
+    
+    nome_restaurante = input('Digite o nome do restaurante: ')
+    restaurantes.append(nome_restaurante)
+    
+    print(Style.BRIGHT + Fore.GREEN + f'O restaurante {nome_restaurante} foi cadastrado com sucesso!\n')
+    pausar()
+
 
 def listar_restaurantes():
-    print( Style.BRIGHT + Fore.LIGHTMAGENTA_EX  + 'Lista de restaurantes\n' )
+    limpar_tela()
+    mostra_subtitulo('Lista de restaurantes')
+    
+    for restaurante in restaurantes:
+        print(f'- {restaurante}')
+    
+    pausar()
+
 
 def ativar_restaurante():
     print( Style.BRIGHT + Fore.LIGHTMAGENTA_EX  + 'Ativar restaurante\n' )
@@ -37,55 +63,44 @@ def exibir_menu():
 
 def erro_input_menu():
     print('Digite um valor válido!\n')
+    pausar( )
+
 
 def escolher_opcao_menu():
     # opcao_escolhida = input('Escolha uma opção')# nesse caso opcao_escolhida será uma str. Por isso ela não podera ser comparada com um int por exemplo.
     while True:
+        limpar_tela()
+        exibir_nome()
+        exibir_menu()
         try:
             # O int() tenta converter o input em inteiro, se algo como um float ou string não numérico forem digitados, ocorre um erro no python! 
             # O try e except lidam com o erro
             opcao_escolhida = int (input(Style.BRIGHT + Fore.LIGHTCYAN_EX + 'Escolha uma opção\n' + Style.RESET_ALL))#aqui há a conversão de str para int antes de associar esse valor a variável.
+            
             if opcao_escolhida == 1:  
                 cadastrar_restaurante()
-                break
+                
             elif opcao_escolhida == 2: 
                 listar_restaurantes()
-                break
+                
             elif opcao_escolhida == 3: 
                 ativar_restaurante()
-                break
+                
             elif opcao_escolhida == 4:
                 finalizar_app()
                 break
             else:
                 erro_input_menu()
                 continue
-        except:
+
+        except ValueError:
             # Captura os erros na conversão do input para inteiro
             erro_input_menu()
     
 
-        
-
-
-def par_ou_impar(): 
-    while True:
-        try:
-            numero= int (input('Digite um número:\n')) 
-            break
-        except ValueError:
-             print('Digite um valor válido!')
-    
-    if( numero % 2 == 0 ):
-        print('numero par')
-    else:
-        print('numero impar')
-
 def main():
     #par_ou_impar()
-    os.system('cls')
-    exibir_nome()
-    exibir_menu()
+
     escolher_opcao_menu()
 
 
